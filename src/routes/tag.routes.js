0bc5b38582ee959ccdd23e5b3075c6ef88ca9b69
Manuel <k84009511@gmail.com>
2026-08-29 18:53:38 -0300

    feat: integracion de middlewares de validacion en las rutas del sistema

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,21 +1,26 @@
 import { Router } from "express";
-import {
-  createTag,
-  getTags,
-  getTagsById,
-  deleteTag,
-  addTagToTask,
-  getTagsByTask
+import { 
+    createTag, 
+    deleteTag, 
+    getTags, 
+    getTagsById, 
+    addTagToTask, 
+    getTagsByTask 
 } from "../controller/tag.controller.js";
+import { 
+    validateCreateTag, 
+    validateTagId, 
+    validateAddTagToTask 
+} from "../middlewares/validations/tag.validations.js";
+import { validateTaskId } from "../middlewares/validations/task.validations.js";
+import { validate } from "../middlewares/validate.js";
 
 export const tagRouter = Router();
 
-
 tagRouter.get('/tags', getTags);
-tagRouter.get('/tags/:id', getTagsById);
-tagRouter.post('/tags', createTag);
-tagRouter.delete('/tags/:id', deleteTag);
+tagRouter.get('/tags/:id', validateTagId, validate, getTagsById);
+tagRouter.post('/tags', validateCreateTag, validate, createTag);
+tagRouter.delete('/tags/:id', validateTagId, validate, deleteTag);
 
-
-tagRouter.post('/tasks/:id/tags/:id', addTagToTask);
-tagRouter.get('/tasks/:id/tags', getTagsByTask);
+tagRouter.post('/tasks/:taskId/tags/:tagId', validateAddTagToTask, validate, addTagToTask);
+tagRouter.get('/tasks/:taskId/tags', validateTaskId, validate, getTagsByTask);
